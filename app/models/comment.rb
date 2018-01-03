2a60f1a4ab64c3d31ce34a053d105962a77e0ce7
humzahchoudry <Humzah.choudry@gmail.com>
2018-01-03 11:27:18 -0500

    added model methods for user and comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,4 +5,13 @@ class Comment < ApplicationRecord
   belongs_to :parent, class_name: "Comment", optional: true
   has_one :movie, through: :group_movie
   has_one :group, through: :group_movie
+
+  def get_rating
+    # Comment.all.select do |c|
+    #   c.parent_id = self.id
+    # end
+    self.children.reduce(0) do |sum, comment|
+      sum += comment.vote
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,33 @@ class User < ApplicationRecord
   has_many :groups, through: :memberships
   has_many :comments
   has_many :admin_groups, class_name: "Group", foreign_key: "admin_id"
+
+  def get_user_groups_comments
+    result = []
+
+    self.groups.each do |group|
+      result << group.comments
+    end
+
+    result.flatten!
+    result.sort_by {|r| r.updated_at}
+
+    # Alternate comment selection
+    # Comment.all.select do |comment|
+    #   comment.group.user_ids.incude?(self.id)
+    # end
+  end
+
+  def available_movies
+    movies = Hash.new
+    self.groups.each do |group|
+      movies[group_id] << group.movies
+    end
+
+
+    movies
+  end
+
+  
+
 end
