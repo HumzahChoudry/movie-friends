eeb8f1fe5efd54ae890ac5ba0400f6fa05847f57
Matt McAlister <matt.mcalister93@gmail.com>
2018-01-04 17:14:19 -0500

    validation for unique group_movie records. each group can only create one discussion thread per movie

--- app/models/group_movie.rb
+++ app/models/group_movie.rb
@@ -3,5 +3,13 @@ class GroupMovie < ApplicationRecord
   belongs_to :movie
   has_many :comments
 
+  validate :group_movie_already_exists?
+
+  def group_movie_already_exists?
+    if GroupMovie.where(movie_id: self.movie_id, group_id: self.group_id)
+      errors.add(:group_id, "is already discussing this movie.")
+    end
+  end
+
 
 end
